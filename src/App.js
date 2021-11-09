198340013015234d17cb996d2af6ddd50e2c5253
Fabrice Niyongabo <ninga.fab@gmail.com>
2021-11-09 04:42:26 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./App.css";
+import Image from "./features/image/Image";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Image />
+    </div>
+  );
+}
